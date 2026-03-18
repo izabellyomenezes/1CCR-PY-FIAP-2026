7f342d40a92ac833e60212d90b0cf933cb3b2c4c
izabellyomenezes <izabelly.omenezes@gmail.com>
2026-03-18 20:46:50 -0300

    aula 2 - variaveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,34 @@
+'''
+
+print ("Hello World")
+
+##function use () to sintax
+
+print (7 + 4)
+
+#variable
+
+name = 'izabelly'
+age = 21
+weight = 61.1
+
+ask_name = str(input('digite seu nome: '))
+ask_age = str(input('digite sua idade: '))
+ask_weight = str(input('digite seu peso: '))
+
+
+print(f'Hello {ask_name}, you are {ask_age} and {ask_weight}kg')
+'''
+
+nome = str(input("Qual seu nome: "))
+print(f'Seja bem-vindx {nome}')
+
+dia = int(input('Qual dia voce nasceu: '))
+mes = int(input('Qual mês você nasceu: '))
+ano = int(input('Qua ano você nasceu: '))
+
+print(f'{dia}/{mes}/{ano}')
+
+num_a= int(input('Digite um numero: '))
+num_b= int(input('Digite outro numero: '))
+print(f'A soma dos seus números são: {num_a + num_b}')
